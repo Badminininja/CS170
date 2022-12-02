e0ef072dda9d179faa969eedcb7306faea508e2f
Joseph M <josephnm911@gmail.com>
2022-12-01 19:32:42 -0800

    learned we can simply change the str to floats

--- AI_Project2.py
+++ AI_Project2.py
@@ -4,11 +4,13 @@ import csv
 print ("hello world")
 file = open('CS170_Small_Data__6.txt')
     #reader = csv.reader(file)
-line = file.readline()
-print (type(line))
-mylist = line.split()
+data = file.readlines() #list of strings that hold all of the data
+
+mylist = data[1].split()
 print(mylist)
-print(mylist[1])
+print(mylist[4])
+floatTest = float(mylist[4])
+print(floatTest)
     #for line in reader:
         #mylist = line.split()
         #print (mylist)

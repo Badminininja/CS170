f4dbf9efb64feee5ad13b4ff601882945e01bbfb
Joseph M <josephnm911@gmail.com>
2022-12-03 16:09:43 -0800

    did the classification of the objects

--- AI_Project2.py
+++ AI_Project2.py
@@ -4,7 +4,7 @@ import random
 current_set = list(())
 random.seed()
 print ("hello world")
-def accuracy_function(data, current_set,feature_to_add): #leave-one-out cross validation
+def cross_validation(data, current_set,feature_to_add): #leave-one-out cross validation
     accuracy = random.random()
     return accuracy
 #testing stub
@@ -18,7 +18,7 @@ def feature_search(data):
         for k in range(1, len(mylist)):
             if k not in current_set:
                 print('considering adding the ' + str(k) + ' feature')
-                temp_accuracy = accuracy_function(data, current_set, k)
+                temp_accuracy = cross_validation(data, current_set, k)
                 if temp_accuracy > best_so_far_accuracy:
                     best_so_far_accuracy = temp_accuracy
                     feature_to_add_at_this_level.append(k)
@@ -31,20 +31,27 @@ def feature_search(data):
 file = open('CS170_Small_Data__6.txt')
     #reader = csv.reader(file)
 data = file.readlines() #list of strings that hold all of the data
-
 mylist = data[0].split()
-
 feature_search(data)
-
 print(mylist)
 print(len(mylist))
 print(mylist[4])
 floatTest = float(mylist[4])
 print(floatTest)
-#print(accuracy_function())
-    #for line in reader:
-        #mylist = line.split()
-        #print (mylist)
-    #   print (line)
-
 file.close()
+
+def accuracy():
+    file = open('CS170_Test__Data.txt')
+    data = file.readlines()
+    mylist = data[0].split()
+    length = len(data)
+    for i in range(0, length):
+        #object_to_classify = 
+        mylist = data[i].split()
+        label_object_to_classify = int(float(mylist[0]))
+
+        print('Looping over i, at the ' + str(i+1) + ' location')
+        print('The ' + str(i+1) + 'th object is in class ' + str(label_object_to_classify)) 
+    file.close()
+
+accuracy()
